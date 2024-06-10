a638b5a00a819b592294894df1bc9e21331e5a03
Stanislav Yudin <syy.stanislav@gmail.com>
2024-06-10 22:03:37 +0300

    removed unused import changed base_url added new way to select date added command.js.scroll_into_view changed capitalized letters in test file name

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.base_url = 'https://demoqa.com'
     driver_options = webdriver.ChromeOptions()
     driver_options.add_argument('--headless')
     driver_options.page_load_strategy = 'eager'
--- tests/test_demoqa_automation_practice_form.py
+++ tests/test_demoqa_automation_practice_form.py
@@ -1,25 +1,32 @@
 import os.path
 
-from selene import browser, have, be
+from selene import browser, have, command
 
 
 def test_complete_todo():
-    browser.open('/')
+    browser.open('/automation-practice-form')
 
     browser.element('#firstName').type('Stanislav')
     browser.element('#lastName').type('Cyberslav')
     browser.element('#userEmail').type('horns.n.hooves@scam.net')
     browser.element('[for="gender-radio-3"]').click()
     browser.element('#userNumber').type('8005553535')
-    browser.element('#dateOfBirthInput').send_keys(u'\ue009' + u'a')
-    browser.element('#dateOfBirthInput').type('01 01 01')
-    browser.element('#dateOfBirthInput').press_enter()
+
+    # browser.element('#dateOfBirthInput').send_keys(u'\ue009' + u'a')  # второй метод установки даты "одним махом"
+    # browser.element('#dateOfBirthInput').type('01 01 01')
+    # browser.element('#dateOfBirthInput').press_enter()
+
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').click().element('[value="2001"]').click()
+    browser.element('.react-datepicker__month-select').click().element('[value="0"]').click()
+    browser.element('.react-datepicker__day--001').click()
     browser.element('#subjectsInput').type('c').press_tab()
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('[for="hobbies-checkbox-2"]').click()
     browser.element('[for="hobbies-checkbox-3"]').click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('pic.png'))
     browser.element('#currentAddress').type('Улица Есенина, дом каруселина')
+    browser.element("#state").perform(command.js.scroll_into_view)
     browser.element('#state').click().element('#react-select-3-option-1').click()
     browser.element('#city').click().element('#react-select-4-option-1').click()
 
